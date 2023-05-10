b5d23a096bbb9ecfb00d6debcf461596b590f86d
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-05-09 17:38:42 -0700

    init commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,63 @@
+// #region METACODE
+// Author: Dustin Bonilla
+// Project Name: 
+
+/*      USER STORY
+ *_______________________________________________________________________________
+ * :: (perspective here)
+ *  
+ *
+ *  
+ * 
+ *_______________________________________________________________________________
+ *      Example - Delete on fill
+ *_______________________________________________________________________________ 
+ * :: A coder
+ * I WANT TO insert some predicate here the client will operate with/on
+ * SO THAT I can align the project to 
+ * 
+*/
+
+/*      ACCEPTANCE CRITERIA
+ *_______________________________________________________________________________
+ * .:
+ * 
+ * 
+ * 
+ * _______________________________________________________________________________
+ *      Template - Delete on fill
+ * _______________________________________________________________________________
+ * .: (Given here)
+ * 
+ * @ means when
+ *  > means then
+ * 
+ * //// marks off completed
+ * 
+ * @ adding to my acceptance criteria // TODO:(1)
+ *  >  
+ * 
+*/
+
+/*      MOCK UP NOTES
+ *_______________________________________________________________________________
+ * 
+ * 
+*/
+
+/*      OTHER NOTES
+ *      GENERAL FLOW OF SITES
+ *_______________________________________________________________________________
+ * 
+ * 
+*/
+// #endregion
+
+//!start
+
+//Debug Printer
+function p(me){console.log(me);}
+
+
+
+//!end
